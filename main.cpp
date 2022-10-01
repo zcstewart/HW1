297fccfe8fe0cb4cd3790696c842e2e3ce32da61
zcstewart <94808022+zcstewart@users.noreply.github.com>
2022-10-01 12:49:33 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+//Homework 1
+
+/*
+ *  Filename:           main.cpp
+ * 
+ *  Purpose:            This program introduces students to their first c++
+ *                      program. Thank god it doesn't say "Hello World".
+ * 
+ *  Author:             Zachary Charles Stewart
+ *  Student ID:         105903414
+ *  Date:               1/17/2017 12:13 PST
+ */
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    cout << "My first C++ program." << endl;
+    
+    return(0);
+}
+
